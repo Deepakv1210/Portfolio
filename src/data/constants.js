6d3e2e6bedc8fe2f30fdf91748d167e1757dd21d
deepak1210 <deepakkumar.v1210@gmail.com>
2024-01-05 19:17:33 -0600

    Created Navigation

---
+++ src/data/constants.js
@@ -0,0 +1,20 @@
+export const Bio = {
+    name: "Deepak Kumar",
+    roles: [
+      "ML Engineer",
+      "Data Engineer",
+      "Software Engineer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/Deepakv1210",
+    resume:
+      "https://drive.google.com/file/d/1hol9yfQ9fJWpr7W6GiC4JtuTgFjBaD7h/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/deepak-kumar-v-907a31146/",
+    twitter: "https://twitter.com/_ScaryNinja_",
+    //insta: "https://www.instagram.com/deepak_v/",
+  };
+  
+
+  
